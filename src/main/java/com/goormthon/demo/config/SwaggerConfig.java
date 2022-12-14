1cd015b51a6301772b8e6cfb472df6e9ed530592
Jeongminyooa <jmjmjmz732002@naver.com>
2022-12-15 01:55:18 +0900

    Feat : Swagger api

---
+++ src/main/java/com/goormthon/demo/config/SwaggerConfig.java
@@ -0,0 +1,27 @@
+package com.goormthon.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1-definition")
+                .pathsToMatch("/api/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI springShopOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Goormthon API")
+                        .description("너는 내 운명 팀의 API 명세서")
+                        .version("v0.0.1"));
+    }
+}
--- src/main/java/com/goormthon/demo/controller/TestController.java
+++ src/main/java/com/goormthon/demo/controller/TestController.java
@@ -1,12 +1,26 @@
 package com.goormthon.demo.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "tests", description = "테스트 API")
 @RestController
+@RequestMapping("/api/tests")
+@ApiResponses({
+        @ApiResponse(responseCode = "200", description = "OK"),
+        @ApiResponse(responseCode = "400", description = "BAD REQUEST"),
+        @ApiResponse(responseCode = "404", description = "NOT FOUND"),
+        @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR")
+})
 public class TestController {
 
-    @GetMapping("/test")
+    @Operation(summary = "get hello", description = "api 테스트 가져오기")
+    @GetMapping("/hello")
     public String greeting() {
         return "hello world!";
     }
