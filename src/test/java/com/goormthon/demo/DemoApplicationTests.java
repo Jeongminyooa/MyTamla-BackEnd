1f8dc85d8ec696d2dcb21e3ea751e75e2a94be32
Jeongminyooa <jmjmjmz732002@naver.com>
2022-12-14 21:43:17 +0900

    Init : first commit

---
+++ src/test/java/com/goormthon/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goormthon.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
