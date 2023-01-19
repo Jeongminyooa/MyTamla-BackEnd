26f061a6da844e927e8c3f390c03f5e1963665c1
Jeongminyooa <jmjmjmz732002@naver.com>
2023-01-19 20:59:01 +0900

    Deploy : code deploy

--- scripts/docker-script.sh
+++ scripts/docker-script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-  DOCKER_IMAGE_NAME=mytamla
+DOCKER_IMAGE_NAME=mytamla
 
-  DOCKER_CONTAINER_NAME=goorm
+DOCKER_CONTAINER_NAME=goorm
 
-  docker build -t ${DOCKER_IMAGE_NAME} . #  프로젝트 루트 경로에 대한 상대경로
+docker build -t ${DOCKER_IMAGE_NAME} . #  프로젝트 루트 경로에 대한 상대경로
 
-  docker run -d -p --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
+docker run -d -p 8081:8080 --name ${DOCKER_CONTAINER_NAME} ${DOCKER_IMAGE_NAME}
